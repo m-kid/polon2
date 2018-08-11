d28fe66a3503a6d80e054bbea799c874bd170570
m-kid <maciej.kidawa2@gmail.com>
2018-08-11 23:56:28 +0200

    dalsze zmiany

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 //przesuwanie strony z konferencją
-/*var frameTop = $('.tlo-1').offset().top;       // get initial position of the element
+var frameTop = $('.tlo-1').offset().top;       // get initial position of the element
 
 $(window).scroll(function() {                  // assign scroll event listener
 
@@ -16,7 +16,13 @@ $(window).scroll(function() {                  // assign scroll event listener
                 $('.konferencja').css({
                 'margin-top': '30vh',
         });
-    } else {                                   // apply position: static
+    } else {                                  // apply position: static
+            $('.tlo-1').css({
+                'position': 'relative',
+            }),
+            $('.konferencja').css({
+                'margin-top':'-70vh',
+            });
     }
 
 });
@@ -42,7 +48,6 @@ if (currentScroll >= insideOneTop) {
 
 //koniec przesuwania strony z konferencją
 
-*/
 
 var fixmeTop = $('.intro-nav').offset().top;       // get initial position of the element
 
@@ -69,7 +74,7 @@ $(window).scroll(function() {                  // assign scroll event listener
     }
 
 });
-
+/*
 var fixmeTopTwo = $('.hiddentitle').offset().top;       // get initial position of the element
 
 $(window).scroll(function() {                  // assign scroll event listener
@@ -93,7 +98,7 @@ $(window).scroll(function() {                  // assign scroll event listener
 
 });
 
-
+*/
 //zamykanie ciastek
 
 $("#cookies-x").click(function() {
@@ -104,205 +109,21 @@ $("#cookies-x").click(function() {
 
 
 
-
-
-
-
-	  function initMap() {
-        var uluru = {lat: 52.237583, lng: 21.018002};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-
-
-	  function initMap2() {
-        var uluru2 = {lat: 52.237584, lng: 21.018003};
-        var map2 = new google.maps.Map(document.getElementById('map2'), {
-          zoom: 14,
-          center: uluru2
-        });
-        var marker2 = new google.maps.Marker({
-          position: uluru2,
-          map: map2
-        });
-      }
-
-
-
-
-
-
-/*zmiana języka na angielski*/
-$("#pl-en-option").click(function() {
-    $(".en").css({
-            'display': 'block',
-    }),
-    $(".en-flex").css({
+//slider
+$(".plan").click(function(){
+       $(".konf-plan-menu").slideToggle("slow"),
+       $(".konf-plan-menu").css({
             'display': 'flex',
-            'display': '-webkit-flex',
-    }),
-
-    $(".pl").css({
-            'display': 'none',
-    }), 
-    $(".pl-flex").css({
-            'display': 'none',
-    })
+            'dispplay': 'webkit-flex',
+       });
+        $("html, body").animate({
+        scrollTop: $(".konferencja").offset().top
+        }, 1000);
 });
 
-$("#pl-en-option-smaller").click(function() {
-    $(".en").css({
-            'display': 'block',
-    }),
-    $(".en-flex").css({
-            'display': 'flex',
-            'display': '-webkit-flex',
-    }),
-
-    $(".pl").css({
-            'display': 'none',
-    }), 
-    $(".pl-flex").css({
-            'display': 'none',
-    })
-});
-
-
-/*koniec zmiany języka na angielski*/
-
-/*zmiana kolorów przycisków po najechaniu myszką - wersja polska*/
-$("#pl-en-option").mouseover(function() {
-    $(".pl-pl-option").css({
-            'background-color': '#BDC3C7',
-    }),
-    $(".pl-en-option").css({
-            'background-color': '#0d3d6b',
-    })
-
-});
-
-
-$("#pl-en-option").mouseout(function() {
-    $(".pl-pl-option").css({
-            'background-color': '#0d3d6b',
-    }),
-    $(".pl-en-option").css({
-            'background-color': '#BDC3C7',
-    })
-
-});
-
-$("#pl-en-option-smaller").mouseover(function() {
-    $(".pl-pl-option-smaller").css({
-            'background-color': '#BDC3C7',
-    }),
-    $(".pl-en-option-smaller").css({
-            'background-color': '#0d3d6b',
-    })
-
-});
-
-
-$("#pl-en-option-smaller").mouseout(function() {
-    $(".pl-pl-option-smaller").css({
-            'background-color': '#0d3d6b',
-    }),
-    $(".pl-en-option-smaller").css({
-            'background-color': '#BDC3C7',
-    })
-
-});
-
-
-/*koniec zmiany kolorów przycisków po najechaniu myszką - wersja polska*/
-
-
-/*zmiana języka na polski*/
-$("#en-pl-option").click(function() {
-
-    $(".en").css({
-            'display': 'none',
-    }),
-    $(".en-flex").css({
-            'display': 'none',
-    }),
-
-    $(".pl").css({
-            'display': 'block',
-    }),
-    $(".pl-flex").css({
-            'display': 'flex',
-            'display': '-webkit-flex'
-    })
-
-});
-
-$("#en-pl-option-smaller").click(function() {
-
-    $(".en").css({
-            'display': 'none',
-    }),
-    $(".en-flex").css({
-            'display': 'none',
-    }),
-
-    $(".pl").css({
-            'display': 'block',
-    }),
-    $(".pl-flex").css({
-            'display': 'flex',
-            'display': '-webkit-flex'
-    })
-
-});
-/*koniec zmiany języka na polski*/
-
-
-/*zmiana kolorów przycisków po najechaniu myszką - wersja angielska*/
-$("#en-pl-option").mouseover(function() {
-    $(".en-pl-option").css({
-            'background-color': '#0d3d6b',
-    }),
-    $(".en-en-option").css({
-            'background-color': '#BDC3C7',
-    })
-
-});
-
-$("#en-pl-option-smaller").mouseover(function() {
-    $(".en-pl-option-smaller").css({
-            'background-color': '#0d3d6b',
-    }),
-    $(".en-en-option-smaller").css({
-            'background-color': '#BDC3C7',
-    })
-
-});
-
-
-$("#en-pl-option").mouseout(function() {
-    $(".en-pl-option").css({
-            'background-color': '#BDC3C7',
-    }),
-    $(".en-en-option").css({
-            'background-color': '#0d3d6b',
-    })
-
-});
-
-$("#en-pl-option-smaller").mouseout(function() {
-    $(".en-pl-option-smaller").css({
-            'background-color': '#BDC3C7',
-    }),
-    $(".en-en-option-smaller").css({
-            'background-color': '#0d3d6b',
-    })
-
-});
-/*koniec zmiany kolorów przycisków po najechaniu myszką - wersja angielska*/
+$(".plan-item-1").click(function(){
+       $(".plan-day1").slideToggle("slow"),
+       $(".plan-day1").siblings.css({
+           'display':'block',       
+        });
+       });
